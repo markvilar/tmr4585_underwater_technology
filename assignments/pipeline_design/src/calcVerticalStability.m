7b1a1f6eb46053130e773c61953b99d64a9077b8
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2019-10-30 13:10:13 +0100

    Removed unused files for task 3.

--- assignments/pipeline_design/src/calcVerticalStability.m
+++
@@ -1,9 +0,0 @@
-function results = calcVerticalStability(segments, soil, rhoIMax, rhoIMin)
-
-nSegs = length(segments);
-outerDs = zeros(nSegs, 1);
-for n = 1:nSegs
-   outerDs(n) = segments(n).calcDiameter();
-end
-
-end
--- assignments/pipeline_design/src/task3.m
+++
@@ -1,5 +0,0 @@
-%% Vertical stability criterion
-
-results = calcVerticalStability(pipeSegments, soil, rhoW, rhoAir);
-
-%% Horizontal stability criterion
